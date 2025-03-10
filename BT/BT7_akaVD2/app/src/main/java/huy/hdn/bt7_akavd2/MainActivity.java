fbc1bd701286152aaa5e4abe03dc03e4a1198467
Bluegagaga <anhemhuyngoc@gmail.com>
2025-03-10 14:27:25 +0700

    Add BT7_akaVD2

---
+++ BT/BT7_akaVD2/app/src/main/java/huy/hdn/bt7_akavd2/MainActivity.java
@@ -0,0 +1,32 @@
+package huy.hdn.bt7_akavd2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnChuyenTrang = findViewById(R.id.btnLogin);
+
+        btnChuyenTrang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iLogin = new Intent(MainActivity.this, ActivityLogin.class);
+
+                startActivity(iLogin);
+            }
+        });
+    }
+}
