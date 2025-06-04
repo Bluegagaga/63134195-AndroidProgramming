cf282620670be48a6658fc30c50020333fffb617
Bluegagaga <anhemhuyngoc@gmail.com>
2025-06-04 10:38:35 +0700

    Update CK63134195

--- CK63134195/app/build.gradle.kts
+++ CK63134195/app/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
 // Import the BoM for the Firebase platform
     implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
-
+    implementation ("com.firebaseui:firebase-ui-database:8.0.0")
     // Add the dependency for the Realtime Database library
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-database")
